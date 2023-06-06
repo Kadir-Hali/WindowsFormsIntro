6cfce87dafb71d9d9ced98088953a457f4c8765e
Kadir Hali <kadirhali07@gmail.com>
2023-06-06 10:59:19 +0300

    6.06.223

---
+++ WindowsFormsIntro/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsIntro
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(maskedTextBox1.Text + "" + maskedTextBox2.Text + "" + maskedTextBox3.Text + "" +
+                               maskedTextBox4);
+        }
+    }
+}
